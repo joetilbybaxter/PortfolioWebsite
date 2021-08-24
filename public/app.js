3e5ed0d235dec6fa957162da034a052501782cc9
JakeTB <jtilbybaxter@gmail.comgit config --global user.name Jakegit config --global user.email jtilbybaxter@gmail.com>
2021-08-24 08:34:02 +0100

    Express js file updated, should be working

--- public/app.js
+++ public/app.js
@@ -1,7 +1,13 @@
 const express = require("express")
+const path = require("path")
 const app = express()
+const publicPath = path.join(__dirname, '..', 'public')
 
-app.use(express.static("public"))
+app.use(express.static("publicPath"))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
 
 app.listen(process.env.PORT || 3000,
     ()=> console.log("Server is running..."))
